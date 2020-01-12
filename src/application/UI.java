c3ed328b4fbc8f4fb274860280a07240379ce8cf
Henrique Tinti de Andrade <henrique.tinti.andrade@gmail.com>
2020-01-12 15:12:45 -0300

    Piece move count

--- src/application/UI.java
+++ src/application/UI.java
@@ -51,7 +51,7 @@ public class UI {
 	public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
 		printBoard(chessMatch.getPieces());
 		System.out.println();
-		printCapturedPiece(captured);
+		printCapturedPieces(captured);
 
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
@@ -105,7 +105,7 @@ public class UI {
 		System.out.print(" ");
 	}
 
-	private static void printCapturedPiece(List<ChessPiece> captured) {
+	private static void printCapturedPieces(List<ChessPiece> captured) {
 		List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE)
 				.collect(Collectors.toList());
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK)
